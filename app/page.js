e1f6a375e71038113619cbe0fab38ecf96640964
Yash Gaur <yashgaur2525@gmail.com>
2023-08-16 18:25:11 +0530

    responsivness

--- app/page.js
+++ app/page.js
@@ -6,14 +6,14 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className='flex flex-col mx-36 my-10 gap-5'>
+    <div className='flex flex-col mx-2 lg:mx-32 my-10 gap-5'>
 
       <div className='flex gap-4  border-b-2 justify-between'>
         <h1 className='font-extrabold  py-8 items-center flex  tracking-tighter h-8 text-3xl text-black'>List of Customers</h1>
         <button className="rounded-md bg-green-600 px-3 text-sm font-semibold text-white shadow-sm hover:bg-green-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"> <Link href="/addCustomer">Add Customer</Link> </button>
       </div>
       x
-      <div className='w-full h-full p-10 bg-gray-200 rounded-xl shadow-md'>
+      <div className='w-full h-full p-2 lg:p-10 bg-gray-200 rounded-xl shadow-md'>
 
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
           <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
