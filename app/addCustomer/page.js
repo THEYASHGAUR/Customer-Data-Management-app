2e59ee9a388c3939549380ed0079edfd5d688db2
Yash Gaur <yashgaur2525@gmail.com>
2023-08-16 18:11:13 +0530

    add customer

---
+++ app/addCustomer/page.js
@@ -0,0 +1,32 @@
+"use client";
+
+export default function add_customer() {
+    const handleInfo = (e) =>{
+        e.preventDefault();
+        console.log("details submited");
+
+    }
+    return (
+        <div className="mx-10 my-10 bg-gray-100 p-10 rounded-lg w-auto">
+
+            <form onSubmit={handleInfo}>
+                <div className="relative z-0 w-full mb-6 group">
+                    <input type="text" name="name" id="name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label for="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name</label>
+                </div>
+                <div className="relative z-0 w-full mb-6 group">
+                    <input type="email" name="email" id="email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label for="floating" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email ID</label>
+                </div>
+                <div className="relative z-0 w-full mb-6 group">
+                    <input type="number" minLength="10" min="10" name="number" id="number" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required />
+                    <label for="number" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone Number</label>
+                </div>
+                
+                
+                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+            </form>
+
+        </div>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,4 +1,8 @@
-import Image from 'next/image'
+"use client";
+
+// import Image from 'next/image'
+import Link from 'next/link';
+
 
 export default function Home() {
   return (
@@ -6,10 +10,96 @@ export default function Home() {
 
       <div className='flex gap-4  border-b-2 justify-between'>
         <h1 className='font-extrabold  py-8 items-center flex  tracking-tighter h-8 text-3xl text-black'>List of Customers</h1>
-        <button className="rounded-md bg-green-600 px-3 text-sm font-semibold text-white shadow-sm hover:bg-green-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">Add Customer</button>
+        <button className="rounded-md bg-green-600 px-3 text-sm font-semibold text-white shadow-sm hover:bg-green-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"> <Link href="/addCustomer">Add Customer</Link> </button>
       </div>
+      x
+      <div className='w-full h-full p-10 bg-gray-200 rounded-xl shadow-md'>
 
-      <div className='w-full h-[500px] bg-gray-200 rounded-xl shadow-md'></div>
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50  dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Email 
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Phone Number
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="bg-white border-b  dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-black whitespace-nowrap">
+                  Apple MacBook Pro 17"
+                </th>
+                <td className="px-6 py-4">
+                  Silver
+                </td>
+                <td className="px-6 py-4">
+                  Laptop
+                </td>
+                
+                <td className="px-6 py-4">
+                  <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+              </tr>
+              <tr className="border-b bg-gray-50  dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                  Microsoft Surface Pro
+                </th>
+                <td className="px-6 py-4">
+                  White
+                </td>
+                <td className="px-6 py-4">
+                  Laptop PC
+                </td>
+                
+                <td className="px-6 py-4">
+                  <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+              </tr>
+              <tr className="bg-white border-b  dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                  Magic Mouse 2
+                </th>
+                <td className="px-6 py-4">
+                  Black
+                </td>
+                <td className="px-6 py-4">
+                  Accessories
+                </td>
+                
+                <td className="px-6 py-4">
+                  <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+              </tr>
+              <tr className="border-b bg-gray-50  dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                  Google Pixel Phone
+                </th>
+                <td className="px-6 py-4">
+                  yashgaur2525@gmail.com
+                </td>
+                <td className="px-6 py-4">
+                  Phone
+                </td>
+                
+                <td className="px-6 py-4">
+                  <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
+                </td>
+              </tr>
+              
+            </tbody>
+          </table>
+        </div>
+
+      </div>
     </div>
   )
 }
